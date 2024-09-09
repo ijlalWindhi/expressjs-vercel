ef19959d21838b631d48e48d3b8eda22e17c257d
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-09-09 15:38:29 +0700

    change(root): change app to index

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (_req, res) => {
+  return res.send("Express on Vercel");
+});
+
+app.get("/ping", (_req, res) => {
+  return res.send("pong 🏓");
+});
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
