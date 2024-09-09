d61d20f122fa3b4476489e8595de5d33f2313b83
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-09-09 16:41:19 +0700

    feat(index): initial route for project and certificate

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { routerProject, routerCertificate } from "./routes/index.js";
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ?? 3001;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (_req, res) => {
-  return res.send("Express on Vercel");
+  res.send("Hello, Dhisa is here!");
 });
 
 app.get("/ping", (_req, res) => {
   return res.send("pong 🏓");
 });
 
+app.use("/project", routerProject);
+app.use("/certificate", routerCertificate);
+
 app.listen(port, () => {
-  return console.log(`Server is listening on ${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
