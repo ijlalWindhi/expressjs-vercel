d857e9efc14d312ef60a1a8ea74f8a518d15bae7
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-09-16 19:15:56 +0700

    refactor(jwt): remove unused authentication logic and clean up code

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,38 +1,7 @@
 import jwt from "jsonwebtoken";
 
-import { errorResponse } from "./responsHandler.js";
-
 export function generateAccessToken(data) {
   return jwt.sign({ data }, process.env.SECRET_KEY, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
   });
-}
-
-export async function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token)
-    return errorResponse(res, 401, "Unauthorized", {
-      code: "UNAUTHORIZED",
-      error: "Token not found",
-    });
-
-  try {
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const user = await prisma.user.findUnique({
-      where: { uuid: decoded.uuid },
-    });
-    if (!user)
-      return errorResponse(res, 403, "Invalid token", {
-        code: "INVALID_TOKEN",
-        error: "Invalid token",
-      });
-    req.user = user;
-    next();
-  } catch (error) {
-    return errorResponse(res, 403, "Invalid token", {
-      code: "INVALID_TOKEN",
-      error: "Invalid token",
-    });
-  }
 }
