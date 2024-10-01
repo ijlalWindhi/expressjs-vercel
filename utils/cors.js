6e8a94024f503beecb9fecfa275af220675ae8e3
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-10-01 17:33:43 +0700

    feat(cors): simplify CORS configuration by allowing all origins

--- utils/cors.js
+++ utils/cors.js
@@ -3,13 +3,14 @@ import cors from "cors";
 const whiteList = ["http://localhost:3001", "https://ijlaldhisa.my.id"];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (whiteList.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"));
+  //   }
+  // },
+  origin: true,
 };
 
 export default cors(corsOptions);
