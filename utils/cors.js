b626f58bdb4db144bfdfdc1fbdaca7b77184d521
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-10-03 10:22:42 +0700

    feat(cors): enhance CORS configuration with dynamic origin checking

--- utils/cors.js
+++ utils/cors.js
@@ -1,9 +1,34 @@
 import cors from "cors";
 
-const whiteList = ["http://localhost:3001", "https://ijlaldhisa.my.id"];
+const whiteList = [
+  "http://localhost:3001",
+  "https://ijlaldhisa.my.id",
+  "https://www.ijlaldhisa.my.id",
+];
 
 const corsOptions = {
-  origin: whiteList,
+  origin: function (origin, callback) {
+    console.log("Request from origin:", origin);
+
+    if (!origin) {
+      console.log("No origin specified");
+      return callback(null, true);
+    }
+
+    const isWhitelisted = whiteList.some((domain) => {
+      const match = origin === domain;
+      console.log(`Checking ${origin} against ${domain}: ${match}`);
+      return match;
+    });
+
+    if (isWhitelisted) {
+      console.log("Origin is whitelisted");
+      callback(null, true);
+    } else {
+      console.log("Origin is not whitelisted");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
